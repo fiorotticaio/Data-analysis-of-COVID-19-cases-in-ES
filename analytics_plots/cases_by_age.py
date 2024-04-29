3e44b4a2a7e8df9428aad749090f0ce972ee119f
fiorotticaio <caiofiorotti@gmail.com>
2024-04-29 18:35:18 -0300

    :bug: improving data visualization on the graph

--- analytics_plots/cases_by_age.py
+++ analytics_plots/cases_by_age.py
@@ -3,32 +3,43 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
+def extract_age(age_str):
+    # Extract only the numerical value of age
+    age_numeric = age_str.split(' ')[0]  # Split by space and get the first element
+    return int(age_numeric)
+
 def plot_cases_by_age(data, cities):
     # Filter the data to include only the desired cities
     data_cities = data[data['Municipio'].isin(cities)]
+    
+    # Convert age values to numeric by extracting the numeric part
+    data_cities['Age'] = data_cities['IdadeNaDataNotificacao'].apply(extract_age)
 
-    # Plot the scatter plot
-    sns.scatterplot(data=data_cities, x='IdadeNaDataNotificacao', y=data_cities.index)
+    # Group the data by age and calculate the total number of cases for each age group
+    cases_by_age = data_cities.groupby('Age').size().reset_index(name='Total Cases')
 
-    # Simplify the display of dates on the x-axis
-    plt.xticks(rotation=70) 
+    # Plot the bar chart
+    plt.figure(figsize=(12, 6))
+    sns.barplot(data=cases_by_age, x='Age', y='Total Cases', palette='viridis')
 
     # Add labels and title
     plt.xlabel('Age')
     plt.ylabel('Number of Cases')
-    plt.title('Scatterplot of Number of Cases by Age for a Group of Cities')
+    plt.title('Number of COVID-19 Cases by Age for a Group of Cities')
 
     # Display the plot
+    plt.xticks(rotation=70) 
+    plt.tight_layout()
     plt.show()
 
 
 if __name__ == "__main__":
     # Load the data from CSV into a DataFrame
     filename = '../MICRODADOS.csv'
-    data = pd.read_csv(filename, delimiter=';', encoding='latin1', nrows=1000)
+    data = pd.read_csv(filename, delimiter=';', encoding='latin1', nrows=10000)
     
     # Choose the desired cities
     cities = ['VITORIA', 'SERRA', 'CARIACICA']
     
-    # Plot the graph of COVID-19 cases and deaths evolution for the specified city
+    # Plot the graph of COVID-19 cases by age for the specified group of cities
     plot_cases_by_age(data, cities)
